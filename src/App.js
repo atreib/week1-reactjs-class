b5f1204e7a1bdc9843c95a7858cbceb5e075bf75
André Treib <andre.schumacher.treib@gmail.com>
2020-06-18 17:49:34 -0300

    use css and image

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import './App.css';
+import logo from './assets/logo.jpg';
 import MyButton from './components/MyButton';
 
 function App () {
+    const [projects, setProjects] = useState(['projeto1', 'projeto2']);
+
+    const handleAddProject = () => {
+        // o que não fazer: projects.push(`Novo projeto ${Date.now()}`);
+        setProjects([...projects, `Novo projeto ${Date.now()}`]);
+    };
+
     return (
         <> 
             <h1>Olá dev</h1>
-            <hr />
             <MyButton title="Botao 1">oi</MyButton>
-            <MyButton title="Botao 2" />
+            <img src={logo} />
+            <br />
+
+            <h2>Meus projetos:</h2>
+            <button onClick={handleAddProject}>Adicionar novo projeto</button>
+            <ul>
+                {projects.map(project => <li key={project}>{project}</li>)}
+            </ul>
         </>
     )
 }
